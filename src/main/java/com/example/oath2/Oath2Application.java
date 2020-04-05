10a03d567161167249d6fed54c32ee588528ef05
Leo Zhong <yunji@uber.com>
2020-04-05 10:33:30 -0700

    basic page

---
+++ src/main/java/com/example/oath2/Oath2Application.java
@@ -0,0 +1,13 @@
+package com.example.oath2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oath2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oath2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oath2/Oath2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oath2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oath2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
